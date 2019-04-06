3fe47db75a8c4e4af3b99b785ed81f67fd8d6a5a
Krzysztof Karol <krzysztof.karol@gmail.com>
2019-04-06 14:31:21 +0200

    Add Map
    Signed-off-by: Krzysztof Karol <krzysztof.karol@gmail.com>

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
-import { NoteList } from "./src/components/NoteList";
+import { Map } from "./src/components/Map";
+// import { NoteList } from "./src/components/NoteList";
 
 export default class App extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <NoteList/>
+        {/*<NoteList/>*/}
+        <Map/>
       </SafeAreaView>
     );
   }
